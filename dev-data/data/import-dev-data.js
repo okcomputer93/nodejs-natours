926eda316a02c519daddca593d89b09160ef9166
Omar J <okcomputer2393@gmail.com>
2021-02-19 15:01:51 -0600

    More about Aggregation Pipelines

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -6,44 +6,44 @@ const Tour = require('../../models/tourModel');
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
 );
 
 mongoose
-    .connect(DB, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('DB connection successful!'));
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 // Read JSON File
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf8')
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf8')
 );
 
 //Import data into database
 const importData = async () => {
-    try {
-        await Tour.create(tours);
-        console.log('Data successfully loaded!');
-    } catch (error) {
-        console.log(error);
-    }
-    process.exit();
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
 };
 
 // Delete all data from DB Collection
 const deleteData = async () => {
-    try {
-        await Tour.deleteMany();
-        console.log('Data successfully loaded!');
-    } catch (error) {
-        console.log(error);
-    }
-    process.exit();
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
 };
 
 if (process.argv[2] === '--import') importData();
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,18 +6,21 @@ const router = express.Router();
 // router.param('id', tourController.checkID);
 
 router
-    .route('/top-5-cheap')
-    .get(tourController.aliasTopTours, tourController.getAllTours);
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tours-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
-    .route('/')
-    .get(tourController.getAllTours)
-    .post(tourController.createTour);
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
 
 router
-    .route('/:id')
-    .get(tourController.getTour)
-    .patch(tourController.updateTour)
-    .delete(tourController.deleteTour);
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
