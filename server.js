d4ad524fa893a0ec503f91b75cf15188aeea938c
Omar J <okcomputer2393@gmail.com>
2021-02-22 17:27:46 -0600

    Debugging with ndb :p

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
 );
 
 mongoose
-    .connect(DB, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('DB connection successful!'));
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 // const testTour = new Tour({
 //     name: 'The Park Camper',
@@ -33,5 +33,5 @@ mongoose
 // Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
