60b69d7bdd209c69d2ccb75498557dda53c3684f
Omar J <okcomputer2393@gmail.com>
2021-02-23 15:42:23 -0600

    Global error handling

--- app.js
+++ app.js
@@ -9,20 +9,15 @@ const app = express();
 // Middlewares
 // console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-    console.log('Hello from the middleware');
-    next();
-});
-
-app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 // app.get('/', (req, res) => {
@@ -46,5 +41,24 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server.`,
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server.`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
