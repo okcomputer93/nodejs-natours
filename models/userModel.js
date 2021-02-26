327d45f7dab1b2838b06068c5a4634d29fbca314
Omar J <okcomputer2393@gmail.com>
2021-02-26 16:36:23 -0600

    Modelling User schema

---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: {
+      validator: function (val) {
+        return validator.isEmail(val);
+      },
+      message: 'Please provide a valid email',
+    },
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
