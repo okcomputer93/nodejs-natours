28095202b7c30c6e86921cd8444178a0d5a98094
Omar J <okcomputer2393@gmail.com>
2021-03-02 15:48:25 -0600

    Authorization: User Roles and Permissions

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -72,8 +72,8 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findOneAndDelete(req.params.id);
-
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+  console.log(req.params.id);
   if (!tour) {
     return next(new AppError('No tour found with that ID', 404));
   }
