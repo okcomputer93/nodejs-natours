0c7b4b1cf1597fd4b456be82f2a52358387f3987
Omar J <okcomputer2393@gmail.com>
2021-03-04 16:59:05 -0600

    Deleting current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,6 +38,15 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user._id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
