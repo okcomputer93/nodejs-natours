f323a18cb21b8e22f28e9d2c2feecc7b6e567865
Omar J <okcomputer2393@gmail.com>
2021-03-04 16:07:45 -0600

    Updating the current user: Data (name, email)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,42 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) =>
+  Object.keys(obj).reduce((acc, el) => {
+    if (allowedFields.includes(el)) {
+      acc[el] = obj[el];
+    }
+    return acc;
+  }, {});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  // We use findByIdAndUpdate since we're not requiring validators to run (in passwords)
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
