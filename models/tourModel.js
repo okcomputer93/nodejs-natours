4439db338adcf7e0aaf50028b8bb941db6e0f5d5
Omar J <okcomputer2393@gmail.com>
2021-03-08 16:49:53 -0600

    Modeling tour guides: child reference

--- models/tourModel.js
+++ models/tourModel.js
@@ -110,7 +110,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
   },
   {
     toJSON: { virtuals: true },
@@ -126,12 +126,6 @@ tourSchema.virtual('durationWeeks').get(function () {
 // NOT IN UPDATE
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
-  next();
-});
-
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
