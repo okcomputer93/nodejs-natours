586439b9bec6f91660ae4ddf28b2dc9574ccde0c
Omar J <okcomputer2393@gmail.com>
2021-03-09 15:29:56 -0600

    Creating and getting reviews

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
-const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 
 const app = express();
@@ -65,6 +66,7 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
 });
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -16,7 +16,7 @@ router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 router
   .route('/')
   .get(authController.protect, tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(authController.protect, tourController.createTour);
 
 router
   .route('/:id')
