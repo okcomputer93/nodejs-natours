5ade8a2a8e0c7e8a92e036a822f516c4f7a7ff2d
Omar J <okcomputer2393@gmail.com>
2021-03-11 15:26:24 -0600

    Fix typo

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,6 +1,5 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const { modelNames } = require('mongoose');
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
