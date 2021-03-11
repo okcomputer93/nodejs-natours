89af9dcef6baae86252c79e384bad6129acc4995
Omar J <okcomputer2393@gmail.com>
2021-03-10 21:59:52 -0600

    Building handler factory functions: delete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
 
 const filterObj = (obj, ...allowedFields) =>
   Object.keys(obj).reduce((acc, el) => {
@@ -81,9 +82,4 @@ exports.updateUser = (req, res) => {
   });
 };
 
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
+exports.deleteUser = factory.deleteOne(User);
