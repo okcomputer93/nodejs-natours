556f59d8f16339b300b51a3cd7310a3bee23fce7
Omar J <okcomputer2393@gmail.com>
2021-04-06 16:41:52 -0500

    CRUD to bookings

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -52,3 +52,13 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   // Redirect without query string
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.getAllBookings = factory.getAll(Booking);
+
+exports.getBooking = factory.getOne(Booking);
+
+exports.createBooking = factory.createOne(Booking);
+
+exports.updateBooking = factory.updateOne(Booking);
+
+exports.deleteBooking = factory.deleteOne(Booking);
