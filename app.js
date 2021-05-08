9757dbdbe4d47d42767c4fcd442ac3ebf70d21d3
Omar J <okcomputer2393@gmail.com>
2021-05-08 16:00:18 -0500

    Implement CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -28,6 +29,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Global Middlewares
+// Implement CORS
+app.use(cors());
+app.options('*', cors());
+
 // Set security HTTP Headers
 app.use(
   helmet({
